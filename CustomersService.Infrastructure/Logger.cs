e71436569565f53e8ce028a5f06c6cb0a9df3cd9
vovchek83 <vovchek83@gmail.com>
2016-09-07 22:59:46 +0300

    Add files via upload

---
+++ CustomersService.Infrastructure/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using Caliburn.Micro;
+
+namespace CustomersService.Infrastructure
+{
+    public class Logger : ILog
+    {
+      //  private readonly AsteaLogViewer.ILogger _log;
+
+        public Logger()
+        {
+          //  LogTraceListener 
+        }
+        public void Error(Exception exception)
+        {
+            Trace.WriteLine(exception);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            Trace.WriteLine(format);
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            Trace.WriteLine(format);
+        }
+    }
+}
